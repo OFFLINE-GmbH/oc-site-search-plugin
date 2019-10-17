a27234e43afabe52c613203c1a561251cd06ae1a
Tobias Kündig <tobias@offline.swiss>
2019-10-17 17:08:35 +0200

    Use meta_title for RainLab.Pages results if available

--- classes/providers/RainlabPagesResultsProvider.php
+++ classes/providers/RainlabPagesResultsProvider.php
@@ -28,7 +28,7 @@ class RainlabPagesResultsProvider extends ResultsProvider
 
         foreach ($this->pages() as $page) {
             // Make this result more relevant, if the query is found in the title
-            $title     = isset($page->viewBag['title']) ? $page->viewBag['title'] : '';
+            $title     = $page->viewBag['meta_title'] ?? $page->viewBag['title'] ?? '';
             $relevance = $this->containsQuery($title) ? 2 : 1;
 
             $result        = new Result($this->query, $relevance);
